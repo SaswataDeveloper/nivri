c8d85cbc611a4bc2d4c9badcb03d11dc7dac2ba7
Saswata <saswatabrandistry@gmail.com>
2026-08-18 16:29:10 +0530

    figma ready

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+
+const mobiletab = document.querySelector(".mobile-tab");
+const navlinks = document.querySelector(".navlinks");
+
+
+mobiletab.addEventListener("click", ()=>{
+    // alert("ok");
+
+    navlinks.classList.toggle("active");
+})
